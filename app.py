1770df36a96f61d847ecfab7fb039a17bd4ba823
Leaks Station <86381194+LKST1@users.noreply.github.com>
2021-12-04 00:18:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ def Bot():
       elif i in olddata :
         ReadfortweetModification = "New Hotfix Modification : \n"
         if olddata[i] != data[i] :
-          print(f"{Fore.LIGHTCYAN_EX}-> Hotfix Modification Detected !!!\n ID : {i}\n{Fore.LIGHTYELLOW_EX}-> OLD : {olddata[i]}\n{Fore.YELLOW}-> NEW : {data[i]}")
-          ReadfortweetModification = ReadfortweetModification + f"-> ID: {i} \n\nOld: {olddata[i]}\n\nNew: {data[i]}\n\n\n"
+          print(f"{Fore.LIGHTCYAN_EX}-> Hotfix Modification Detected !!!\n>> ID : {i}\n{Fore.LIGHTYELLOW_EX}-> OLD : {olddata[i]}\n{Fore.YELLOW}-> NEW : {data[i]}")
+          ReadfortweetModification = ReadfortweetModification + f"ID: {i} \n\nOld: {olddata[i]}\n\nNew: {data[i]}\n\n\n"
           tweeting(Readfortweet=ReadfortweetModification,text="-> Hotfix Modification Tweeted successfully")
     Odata = open("data/oldhotfixes.json","w+")
     json.dump(data,Odata,indent=3)
 
-if "HI"== "HI":
+if "HI"=="HI":
   while True:
     try:
       Bot()
