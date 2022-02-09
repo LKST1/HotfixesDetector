07502847f27f807e70ee6839754c8e91c24c4972
Leaks Station <86381194+LKST1@users.noreply.github.com>
2022-02-09 13:19:05 +0200

    Update app.py
    - The New Update to Prevent Getting the Tweeting error , Because Of the Passing the MAX Limit of twitter tweet ,so the new way is :
    
    1- IF the tweet with all new hotfixes combined Is *less* than 280 character  : It will tweet it Normally
    __________________________________________________________________________________________________________
    2- IF the tweet with all new hotfixes combined Is *More* than 280 character  : It will tweet every single Hotfix By Itself
    __________________________________________________________________________________________________________
    
     it's useful to prevent getting any error from the bot , It could Spam if many hotfixes got added , and  that's what im working On now to get Idea for this ❤️

--- app.py
+++ app.py
@@ -14,42 +14,47 @@ consumer_key = tokens['data']['API_key'] #TWITTER
 consumer_secret_key = tokens['data']['API_Secret_Key'] #TWITTER
 access_token = tokens['data']['Access_Token'] #TWITTER
 access_token_secret = tokens['data']['Access_Token_Secret'] #TWITTER
-if consumer_key == "" or consumer_secret_key == "" or access_token == "" or access_token_secret == "" :
-  print(f"{Fore.RED}-> Please Check Your Tokens in [youtTokens.json]- missing token !!")
-  time.sleep(5)
-  exit()
+if consumer_key == "" or consumer_secret_key == "" or access_token == "" or access_token_secret == "" : print(f"{Fore.RED}-> Please Check Your Tokens in [youtTokens.json]- missing token !!"),time.sleep(5),exit()
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 def tweeting(Readfortweet,text="-> New Hotfix Tweeted successfully"):
-  try:
-      api.update_status(Readfortweet)
-      print(f"{Fore.BLUE}{text}\n\n")
-  except:
-    print(f"{Fore.RED}-> Failed to tweet")
+  try: api.update_status(Readfortweet),print(f"{Fore.BLUE}{text}\n\n")
+  except: print(f"{Fore.RED}-> Failed to tweet")
 #######################################################################
 def Bot():
   Tweet = False
   data = get("https://benbot.app/api/v1/hotfixes").json()[""]
   Odata = open("data/oldhotfixes.json","r+")
   olddata = json.load(Odata)
-  if data == olddata :
+  if data == {} or data == None :
+    print(f"{Fore.RED} Not checking The Api is Empty ( The bot will start Checking again when the api back) ".title())
+  elif data == olddata :
     print(f"{Fore.LIGHTGREEN_EX}>>> Searching For New Hotfix!")
-
   else:
     ReadfortweetNew = "New Hotfixes: \n"
     print(f"{Fore.CYAN}-> New Hotfix Detected !!!")
     for i in data :
-      if i not in olddata :
-        Tweet = True
+      if i not in olddata : 
+        Tweet = i not in olddata
         print(f"New Hotfix : {Fore.LIGHTYELLOW_EX}-> ID :{i}")
         dataget = data[i] 
         ReadfortweetNew = ReadfortweetNew + f"• {dataget}\n"
-    if Tweet == True :
-      tweeting(Readfortweet=ReadfortweetNew)
-    else :
-      pass
+    #To tweet every single one Because we can tweet all of them one time , due to the max limit of twtter 280 character ONLY
+    if len(ReadfortweetNew) > 280 :
+      for i in data :
+        ReadfortweetNew = "New Hotfixes: \n"
+        if i not in olddata :
+          dataget = data[i] 
+          ReadfortweetNew = ReadfortweetNew + f"• {dataget}\n"
+          print(f"New Hotfix : {Fore.LIGHTYELLOW_EX}-> ID :{i}")
+          tweeting(Readfortweet=ReadfortweetNew)
+    #Here will make sure if it's Equal or Less the the Limit Of twitter , iF less than the limit, It will post it all in one tweet
+    elif len(ReadfortweetNew) <= 280 :
+      if Tweet == True :
+        tweeting(Readfortweet=ReadfortweetNew)
+    #Here Checking for new Modification Of any Hotix :D
     for i in data :
       if i in olddata :
         ReadfortweetModification = "New Hotfix Modification : \n"
